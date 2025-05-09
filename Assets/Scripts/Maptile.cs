dbc0956d2b160c07fe76dcdf0c5b3bd52b3b29f0
dlckdtjs <117832301+dlckdtjs@users.noreply.github.com>
2025-05-09 20:46:14 +0900

    Maptile 수정
    Maptile 수정

--- Assets/Scripts/Maptile.cs
+++ Assets/Scripts/Maptile.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
@@ -25,15 +26,13 @@ public class Maptile : MonoBehaviour
         spawnArea.isTrigger = true;
     }
 
+   // 만약 Monster 스크립트가 OnDeath 이벤트가 없으면, 해당 스크립트의 Die()나 OnDestroy() 안에 DungeonManager.Instance.UnregisterMonster(); 를 직접 넣어 주세요.
 
 
-
-    public void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (hasSpawned || !other.CompareTag("Player"))
-            return;
-
-
+        if (hasSpawned || !other.CompareTag("Player")) return;
+        hasSpawned = true;
 
         Bounds b = spawnArea.bounds;
         for (int i = 0; i < monstersPerTile; i++)
@@ -42,11 +41,30 @@ public class Maptile : MonoBehaviour
                 Random.Range(b.min.x, b.max.x),
                 Random.Range(b.min.y, b.max.y)
             );
+            // 1) 몬스터 인스턴스화
+            var go = Instantiate(
+                monsterPrefabs[Random.Range(0, monsterPrefabs.Length)],
+                pos,
+                Quaternion.identity,
+                transform
+            );
 
-            var prefab = monsterPrefabs[Random.Range(0, monsterPrefabs.Length)];
-            Instantiate(prefab, pos, Quaternion.identity, transform);
+            // 2) DungeonManager에 등록
+            DungeonManager.Instance.RegisterMonster();
+
+            // 3) 몬스터 죽음 이벤트 연결
+            var monster = go.GetComponent<Monster>();
+            if (monster != null)
+            {
+                monster.OnDeath += () =>
+                {
+                    DungeonManager.Instance.UnregisterMonster();
+                };
+            }
+            else
+            {
+                // Monster 스크립트가 없다면, 대신 태그 기반으로 OnDestroy에서 처리하게 할 수도 있습니다.
+            }
         }
-        hasSpawned = true;
-
     }
 }
