24d1582765f37745ae8580f7a3a7e2c491342c9b
Hyeon O <115928656+gusdh8380@users.noreply.github.com>
2025-05-09 20:54:13 +0900

    fix : Merge 충돌 수정

--- Assets/Scripts/DataBase/MonsterData_ho.cs
+++ Assets/Scripts/DataBase/MonsterData_ho.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(menuName ="Data/MonsterData")]
-public class MonsterData : ScriptableObject
+public class MonsterData_ho : ScriptableObject
 {
    //Todo : 병함 후 구현
 }
