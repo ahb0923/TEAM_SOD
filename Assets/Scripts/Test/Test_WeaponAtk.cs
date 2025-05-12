0fdb02729090844c2c073e81fcf61e548078b037
Hyeon O <115928656+gusdh8380@users.noreply.github.com>
2025-05-12 23:18:39 +0900

    fix : 파일 유니코드 설정 변경

--- Assets/Scripts/Test/Test_WeaponAtk.cs
+++ Assets/Scripts/Test/Test_WeaponAtk.cs
@@ -1,7 +1,9 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+
+//무기 테스트용 스크립트
 
 public class Test_WeaponAtk : MonoBehaviour
 {
@@ -11,7 +13,7 @@ public class Test_WeaponAtk : MonoBehaviour
    
     public RangeWeapon rangeWeapon;
     public MeleeWeapon meleeWeapon;
-    public Transform t;
+    public Transform t; //테스트용 타겟
     private void Start()
     { 
 
@@ -20,13 +22,17 @@ public class Test_WeaponAtk : MonoBehaviour
     }
     void Update()
     {
+        //플레이어에서 활 발사 로직 작성하실때
+        //도움이 되길 바라며...
         if (rangeWeapon.data.attackRange >= Vector2.Distance(r.transform.position, t.position))
         {
             rangeWeapon.Attack(t.position);
         }
         else { rangeWeapon.animator.SetBool("IsAttack", false); }
-        
+        //활은 사거리 계산이 외부에 있어 애니메이션 종료를 외부에서 호출해야할 것 같아요...
 
+        //반면 무기는 그냥 Update에서 호출하셔도 괜찮습니다.
+        //메소드 내부에 사거리 계산을 하도록 하였습니다.
         meleeWeapon.Attack(t.position);
 
 
