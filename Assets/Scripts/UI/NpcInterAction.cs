df973a5cc12aee1b8f48a1161a50530e259c2944
dlckdtjs <117832301+dlckdtjs@users.noreply.github.com>
2025-05-14 17:55:07 +0900

    백그라운드 레이어 적용
    백그라운드 레이어 적용

--- Assets/Scripts/UI/NpcInterAction.cs
+++ Assets/Scripts/UI/NpcInterAction.cs
@@ -18,7 +18,10 @@ public class NpcInterAction : BaseInterAction
 
         };
 
-       
+        model.ButtonActions["Button_Enter"] = () =>
+        {
+
+        };
 
         UIManager.Instance.ShowPanel(model);
 
