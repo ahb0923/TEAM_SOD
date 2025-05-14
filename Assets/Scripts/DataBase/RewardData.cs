80c6c4273d731e8ee946ae8121f1883c2ade4482
Hyobin <loastkeas@gmail.com>
2025-05-14 22:29:46 +0900

    RewadData 틀 생성
    보상 데이터 틀 생성

--- Assets/Scripts/DataBase/RewardData.cs
+++ Assets/Scripts/DataBase/RewardData.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class RewardData : MonoBehaviour
+public enum REWARD_GRADE
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    NORMAL,
+    RARE,
+    UNIQUE
+}
+[CreateAssetMenu(menuName = "Data/RewardData")]
+public class RewardData : ScriptableObject
+{
+    [Header("데이터 기본 속성")]
+    [SerializeField]
+    public int code;
+    [SerializeField]
+    public REWARD_GRADE grade;
+    [SerializeField]
+    public string title;
+    [SerializeField]
+    public string text;
+    [SerializeField]
+    public Sprite image;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("플레이어 증감 수치")]
+    [SerializeField]
+    public float hp;        // 이 부분은 힐 느낌 으로 쓸려고 합니다
+    [SerializeField]
+    public float maxHp;     // 이 부분은 최대 체력 느낌 증강
+    [SerializeField]
+    public int atk;
+    [SerializeField]
+    public int def;
+    [SerializeField]
+    public float playerMoveSpeed;
+    [SerializeField]
+    public float playerCriChance;
+    [SerializeField]
+    public float playerCriMulti;
+    [SerializeField]
+    public float playerInvisible;
+
+    [Header("무기 증감 수치")]
+    [SerializeField]
+    public float weaponPower;
+    [SerializeField]
+    public float weaponSpeed;
+    [SerializeField]
+    public float weaponRange;
+    [SerializeField]
+    public int weaponShotCount;
 }
